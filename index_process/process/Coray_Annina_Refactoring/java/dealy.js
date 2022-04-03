70116b17250052acba7c760738bc889983a9127a
Exelie <91537907+Exelie@users.noreply.github.com>
2022-04-04 01:07:49 +0200

    pause change

--- index_process/process/Coray_Annina_Refactoring/java/dealy.js
+++ index_process/process/Coray_Annina_Refactoring/java/dealy.js
@@ -1,16 +1,15 @@
+var play = true;
 
-var play = false; 
 function play_stop() {
-    
-if (play == false){
-    play = true;
-    document.getElementById("song").play();
-    console.log("test");
-}
-else {
-    play = false;
-    document.getElementById("song").pause();
-    console.log("test2");
-}
-return
+
+    if (play == false) {
+        play = true;
+        document.getElementById("song").play();
+        console.log("test");
+    } else {
+        play = false;
+        document.getElementById("song").pause();
+        console.log("test2");
+    }
+    return
 };
