1a22d280f7a4dc3cd18536b3186f82a2b39a96ab
Exelie <91537907+Exelie@users.noreply.github.com>
2022-04-04 00:59:51 +0200

    buttonback

---
+++ index_process/process/Coray_Annina_Refactoring/java/dealy.js
@@ -0,0 +1,16 @@
+
+var play = false; 
+function play_stop() {
+    
+if (play == false){
+    play = true;
+    document.getElementById("song").play();
+    console.log("test");
+}
+else {
+    play = false;
+    document.getElementById("song").pause();
+    console.log("test2");
+}
+return
+};
